bb688cf43be9ba55130def98d2925091c86bd41a
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-18 19:50:07 -0300

    feat: criada classe de erro notfound

--- app/backend/src/middlewares/errorMiddleware.ts
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -9,6 +9,9 @@ const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
     case 'UnauthorizedError':
       res.status(401).json({ message });
       break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
     default:
       res.status(500).json({ message });
   }
