9aa68601d99cb878a24bf32e7fcaf38b46d5ea7d
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-18 16:26:05 -0300

    feat: criado erro 'unauthorized'

--- app/backend/src/middlewares/errorMiddleware.ts
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -3,8 +3,11 @@ import { ErrorRequestHandler } from 'express';
 const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
   const { name, message } = err;
   switch (name) {
-    case 'ValidationError':      
+    case 'ValidationError':
       res.status(400).json({ message: err.details[0].message });
+      break;
+    case 'UnauthorizedError':
+      res.status(401).json({ message });
       break;
     default:
       res.status(500).json({ message });
---
+++ app/backend/src/services/errors/unauthorized.error.ts
@@ -0,0 +1,3 @@
+export default class UnauthorizedError extends Error {
+  name = 'UnauthorizedError';
+}
