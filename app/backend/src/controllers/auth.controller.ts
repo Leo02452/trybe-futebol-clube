464c827592976303a4f05b43674b28548a18f425
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-18 19:54:44 -0300

    feat: implementada funcionalidade de validar token
    Requisito 12 concluído

--- app/backend/src/controllers/auth.controller.ts
+++ app/backend/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import ILoginBody from '../interfaces/ILoginBody';
 import IAuthService from '../interfaces/IAuthService';
+import NotFoundError from '../services/errors/notfound.error';
 
 export default class AuthController {
   constructor(
@@ -13,5 +14,14 @@ export default class AuthController {
     const token = await this.authService.login(body.email, body.password);
 
     res.status(200).json({ token });
+  }
+
+  async validate(req: Request, res: Response) {
+    const token = req.headers?.authorization;
+    if (!token) {
+      throw new NotFoundError('Token not found');
+    }
+    const role = await this.authService.getRole(token);
+    res.status(200).json(role);
   }
 }
--- app/backend/src/routes/auth.route.ts
+++ app/backend/src/routes/auth.route.ts
@@ -3,6 +3,7 @@ import authController from './main';
 
 const router = Router();
 
+router.get('/validate', (req: Request, res: Response) => authController.validate(req, res));
 router.post('/', (req: Request, res: Response) => authController.login(req, res));
 
 export default router;
