052d269965c257dc4ffd589b685891e8673f0c70
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-19 12:11:35 -0300

    feat: criada entidade times
    Criado migration, model e seeder para entidade
    
    Requisito 14 concluído

---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,26 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Team;
