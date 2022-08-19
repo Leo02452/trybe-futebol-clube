af4929fe6f646d94be494e43d70ee50821ad0e19
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-19 19:34:18 -0300

    feat: implementada func de filtar partidas
    Funcionalidade filtra partidas finalizadas ou em andamento
    
    Requisito 20 e 21 concluídos

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -11,4 +11,12 @@ export default class MatchesController {
 
     res.status(200).json(matches);
   }
+
+  async filterByProgress(req: Request, res: Response): Promise<void> {
+    const { inProgress } = await this.matchesService.validateQuery(req.query);
+    const filteredMatches = await this.matchesService
+      .filterByProgress(JSON.parse(inProgress));
+
+    res.status(200).json(filteredMatches);
+  }
 }
--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -3,6 +3,8 @@ import { matchesController } from './main';
 
 const router = Router();
 
+router.get('/?inProgress', (req: Request, res: Response) => matchesController
+  .filterByProgress(req, res));
 router.get('/', (req: Request, res: Response) => matchesController.list(req, res));
 
 export default router;
