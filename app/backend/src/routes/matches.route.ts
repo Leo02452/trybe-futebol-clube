4f11dbc3b4921a661cfbf7dcd673d8dd4f639bd0
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-22 19:43:30 -0300

    feat: rota para funcionalidade de criar partidas

--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/?inProgress', (req: Request, res: Response) => matchesController
   .filterByProgress(req, res));
 router.get('/', (req: Request, res: Response) => matchesController.list(req, res));
+router.post('/', (req: Request, res: Response) => matchesController.create(req, res));
 
 export default router;
