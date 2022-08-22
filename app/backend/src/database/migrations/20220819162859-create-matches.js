5fd4ecac2510e309a0799fc4238ac3730452d736
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-22 19:18:03 -0300

    feat: criado valor padrão para coluna inProgress
    Coluna está na tabela matches e foi setado valor padrão como 'true'

--- app/backend/src/database/migrations/20220819162859-create-matches.js
+++ app/backend/src/database/migrations/20220819162859-create-matches.js
@@ -39,7 +39,7 @@ module.exports = {
       },
       inProgress: {
         type: Sequelize.BOOLEAN,
-        allowNull: false,
+        defaultValue: true,
         field: 'in_progress'
       }
     });
