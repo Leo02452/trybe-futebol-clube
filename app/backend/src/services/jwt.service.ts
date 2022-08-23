cacdbefb3214eb7b5c93bd6958fc1d8c17b257f8
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-23 10:48:06 -0300

    perf: função que valida o token do usuario
    Função foi melhorada para lançar erro caso token não seja valido

--- app/backend/src/services/jwt.service.ts
+++ app/backend/src/services/jwt.service.ts
@@ -1,6 +1,7 @@
 import { sign, verify } from 'jsonwebtoken';
 import 'dotenv/config';
 import { IUserWithoutPassword } from '../interfaces/IUser';
+import UnauthorizedError from './errors/unauthorized.error';
 
 const { JWT_SECRET } = process.env;
 
@@ -9,7 +10,13 @@ const secret = JWT_SECRET || 'jwt_secret';
 export default class JwtService {
   public signUp = async (payload: IUserWithoutPassword): Promise<string> => sign(payload, secret);
   public verifyToken = async (token: string): Promise<IUserWithoutPassword> => {
-    const userData = verify(token, secret);
-    return userData as IUserWithoutPassword;
+    let userData = {} as IUserWithoutPassword;
+    verify(token, secret, (error, decode) => {
+      if (error) {
+        throw new UnauthorizedError('Token must be a valid token');
+      }
+      userData = decode as IUserWithoutPassword;
+    });
+    return userData;
   };
 }
