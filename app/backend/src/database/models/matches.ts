0d962b45b88434fb295cb953cd778114666f4901
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-27 15:10:54 -0300

    feat: add associação entre modelos
    Criada associação para acessar partidas através dos times.

--- app/backend/src/database/models/matches.ts
+++ app/backend/src/database/models/matches.ts
@@ -42,5 +42,7 @@ Match.init({
 
 Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
 Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeMatches' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayMatches' });
 
 export default Match;
