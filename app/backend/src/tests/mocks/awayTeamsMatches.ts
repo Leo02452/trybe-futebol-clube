bc20452ec55fcf947d4211da486cf56ec6edf4d3
Leo Araujo <leonardo_02452@hotmail.com>
2022-08-27 23:58:54 -0300

    test: criados mocks de leaderboard
    mock para
    partida em casa,
    partida fora,
    classificação em casa,
    classificação fora e
    classificaçao geral

---
+++ app/backend/src/tests/mocks/awayTeamsMatches.ts
@@ -0,0 +1,242 @@
+export const awayTeamsMatches = [
+  {
+    "teamName": "Grêmio",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 3
+      },
+      {
+        "homeTeamGoals": 4,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Santos",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Napoli-SC",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 5,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Bahia",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Minas Brasília",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Real Brasília",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Ferroviária",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Avaí/Kindermann",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Palmeiras",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 3
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 4
+      }
+    ]
+  },
+  {
+    "teamName": "Internacional",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 2
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Botafogo",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Corinthians",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 4
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Cruzeiro",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 4,
+        "awayTeamGoals": 2
+      },
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 3
+      }
+    ]
+  },
+  {
+    "teamName": "São Paulo",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 3
+      },
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "São José-SP",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Flamengo",
+    "awayMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      }
+    ]
+  }
+]
---
+++ app/backend/src/tests/mocks/homeTeamsMatches.ts
@@ -0,0 +1,242 @@
+export const homeTeamsMatches = [
+  {
+    "teamName": "São Paulo",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Internacional",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 4
+      },
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Corinthians",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Botafogo",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 4
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Flamengo",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Cruzeiro",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Palmeiras",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      },
+      {
+        "homeTeamGoals": 4,
+        "awayTeamGoals": 2
+      },
+      {
+        "homeTeamGoals": 4,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "São José-SP",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 3
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Avaí/Kindermann",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 3
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 3
+      },
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Bahia",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 2
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Real Brasília",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Ferroviária",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 3,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Grêmio",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Santos",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 5,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Minas Brasília",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 1
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      },
+      {
+        "homeTeamGoals": 1,
+        "awayTeamGoals": 3
+      }
+    ]
+  },
+  {
+    "teamName": "Napoli-SC",
+    "homeMatches": [
+      {
+        "homeTeamGoals": 0,
+        "awayTeamGoals": 0
+      },
+      {
+        "homeTeamGoals": 2,
+        "awayTeamGoals": 2
+      }
+    ]
+  }
+]
